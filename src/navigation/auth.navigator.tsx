beb942fe13e52682907b296e1b85576f828e45a6
Hussain Chhatriwala <hussain4786@gmail.com>
2020-12-22 12:14:13 +0530

    Fixed merge issues

--- src/navigation/auth.navigator.tsx
+++ src/navigation/auth.navigator.tsx
@@ -5,7 +5,7 @@ import Login from '@/screens/Auth/Login/Login';
 import Welcome from '@/screens/Auth/Welcome/Welcome';
 import Otp from '@/screens/Auth/Otp/Otp';
 import Password from '@/screens/Auth/Password/Password';
-
+import {Platform  } from "react-native";
 const {Navigator, Screen} = createStackNavigator();
      
 export const AuthStack = () => {
