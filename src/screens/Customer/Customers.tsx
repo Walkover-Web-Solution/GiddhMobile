bab3c2d280cd08ff87951b0f11a625f80b7f842c
Sulbha-Mishra29 <sulbha@whozzat.com>
2021-05-07 23:22:59 +0530

    Solved minor bugs and validate number and email id and modify design of customer screen

--- src/screens/Customer/Customers.tsx
+++ src/screens/Customer/Customers.tsx
@@ -277,14 +277,14 @@ export class Customers extends React.Component<Props> {
     return (
       <View style={{ marginHorizontal: 15, marginVertical: 10, marginRight: 20, overflow: 'hidden' }}>
         <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingRight: 20, }}>
-          <View style={{ width: "80%", }}>
+          <View style={{ width: "74%", }}>
             <View style={{ flexDirection: 'row', alignItems: "flex-end" }}>
               <Text style={{ color: '#1c1c1c', paddingRight: 5, marginTop: 10 }} >Set Currency (account)</Text>
               <Foundation name="info" size={16} color="#b2b2b2" />
             </View>
             {/* <Text style={{ color: '#808080', fontSize: 12, maxWidth: '80%', }}>Choose currency for opening Balance eg.INR  </Text> */}
           </View>
-          <View style={{ ...styles.rowContainer, marginStart: -24, marginTop: 5, paddingHorizontal: 10, marginHorizontal: 10, height: 40, width: "34%", borderWidth: 1, borderColor: '#d9d9d9', justifyContent: 'space-between', overflow: 'hidden' }}>
+          <View style={{ ...styles.rowContainer, marginTop: 5, paddingHorizontal: 10, height: 40, width: "30%", borderWidth: 1, borderColor: '#d9d9d9', justifyContent: 'space-between', overflow: 'hidden' }}>
             <Dropdown
               ref={(ref) => this.state.creditPeriodRef = ref}
               textStyle={{ color: '#808080' }}
@@ -298,7 +298,7 @@ export class Customers extends React.Component<Props> {
               }}
               onSelect={(idx, value) => this.setState({ selectedCurrency: value.code })}
 
-              dropdownStyle={{ width: '29%', marginTop: 11, borderRadius: 8, marginRight: -73 }}
+              dropdownStyle={{ width: '29%', marginTop: 11, borderRadius: 8, }}
               dropdownTextStyle={{ color: '#1C1C1C' }}
               renderRow={(options) => {
                 return (<Text style={{ padding: 13, color: '#1C1C1C' }}>{options.code}</Text>);
@@ -316,7 +316,7 @@ export class Customers extends React.Component<Props> {
           </View>
         </View>
         <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingRight: 20, marginTop: 10 }}>
-          <View style={{ width: "80%" }}>
+          <View style={{ width: "74%", }}>
             <View style={{ flexDirection: 'row', alignItems: "flex-end" }}>
               <Text style={{ color: '#1c1c1c', paddingRight: 5, marginTop: 10 }} >Foreign Opening Balance</Text>
               <Foundation name="info" size={16} color="#b2b2b2" />
@@ -328,15 +328,15 @@ export class Customers extends React.Component<Props> {
             onChangeText={(val) => { this.setState({ foreignOpeningBalance: val }) }}
             value={this.state.foreignOpeningBalance}
             placeholder="Amount"
-            style={{ borderWidth: 1, borderColor: "#d9d9d9", height: '80%', width: "34%", paddingStart: 10, marginStart: -24, }} />
+            style={{ borderWidth: 1, borderColor: "#d9d9d9", width: "30%", height: '80%', paddingStart: 10, }} />
         </View>
-        <View style={{ flexDirection: 'row', justifyContent: "space-between", marginTop: 20 }}>
-          <View style={{ width: "50%" }}>
+        <View style={{ flexDirection: 'row', justifyContent: "space-between", marginTop: 5 }}>
+          <View style={{ width: "70%", }}>
             <View style={{ flexDirection: 'row', alignItems: "flex-end" }}>
               <Text style={{ color: '#1c1c1c', paddingRight: 5, marginTop: 10 }} >Opening Balance</Text>
               <Foundation name="info" size={16} color="#b2b2b2" />
             </View>
-            <RadioForm
+            {/* <RadioForm
               formHorizontal={true}
               initial={0}
               animation={true}
@@ -346,7 +346,7 @@ export class Customers extends React.Component<Props> {
                 this.radio_props.map((obj, i) => (
                   <RadioButton labelHorizontal={true} key={i} style={{ alignItems: 'center' }} >
                     {/*  You can set RadioButtonLabel before RadioButtonInput */}
-                    <RadioButtonInput
+            {/* <RadioButtonInput
                       obj={obj}
                       index={i}
                       isSelected={this.state.radioBtn === i}
@@ -368,9 +368,8 @@ export class Customers extends React.Component<Props> {
                       labelWrapStyle={{ marginRight: 10, marginTop: 10 }}
                     />
                   </RadioButton>
-                ))
-              }
-            </RadioForm>
+                )) }*/}
+            {/* </RadioForm> */}
           </View>
           <TextInput
             keyboardType="number-pad"
@@ -379,27 +378,55 @@ export class Customers extends React.Component<Props> {
             }}
             value={this.state.openingBalance}
             placeholder="Amount"
-            style={{ borderWidth: 1, borderColor: "#d9d9d9", height: '70%', width: "32.5%", paddingStart: 10, marginTop: 5 }} />
+            style={{ borderWidth: 1, width: "30%", borderColor: "#d9d9d9", height: '70%', paddingStart: 10, marginTop: 5 }} />
         </View>
       </View>
     );
   }
 
   isCreateButtonVisible = () => {
-    if (this.state.partyName && this.state.savedAddress.state_billing) {
+    if (this.state.partyName && this.state.partyType != "Party Type*" && this.state.savedAddress.state_billing) {
       return true;
     } else {
       return false;
     }
   }
 
+  validateMobileNumber = () => {
+    if (this.state.contactNumber == "") {
+      return true
+    }
+    var pattern = new RegExp(/^[0-9\b]+$/);
+    if (!pattern.test(this.state.contactNumber)) {
+      Alert.alert("Error", 'Please enter only number.', [{ style: "destructive", onPress: () => console.log("alert destroyed") }]);
+      return false;
+    } else if (this.state.contactNumber.length != 10) {
+      Alert.alert("Error", 'Please enter valid phone number.', [{ style: "destructive", onPress: () => console.log("alert destroyed") }]);
+      return false;
+    }
+    return true
+  }
+
+  validateEmail = () => {
+    if (this.state.emailId == "") {
+      return true
+    }
+    const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    if (!expression.test(String(this.state.emailId).toLowerCase())) {
+      Alert.alert("Error", 'Please enter correct email-address.', [{ style: "destructive", onPress: () => console.log("alert destroyed") }]);
+      return false;
+    }
+    return true
+  }
   genrateCustomer = () => {
     if (!this.state.partyName) {
       Alert.alert("Error", 'Please select a party.', [{ style: "destructive", onPress: () => console.log("alert destroyed") }]);
     } else if (!this.state.savedAddress.state_billing) {
       Alert.alert("Error", 'Please select state to proceed.', [{ style: "destructive", onPress: () => console.log("alert destroyed") }]);
     } else {
-      this.createCustomerRequest();
+      if (this.validateMobileNumber() && this.validateEmail()) {
+        this.createCustomerRequest();
+      }
     }
   }
 
@@ -445,7 +472,7 @@ export class Customers extends React.Component<Props> {
       console.log('Create Customer postBody is', JSON.stringify(postBody));
       const results = await CustomerService.createCustomer(postBody);
       if (results.status == "success") {
-        DeviceEventEmitter.emit(APP_EVENTS.CustomerCreated, {});
+        await DeviceEventEmitter.emit(APP_EVENTS.CustomerCreated, {});
         await this.resetState();
         await this.setState({ successDialog: true, });
         await this.getAllDeatils()
