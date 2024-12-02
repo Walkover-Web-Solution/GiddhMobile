93df82d1509f54501d1734c2c63e29e600256917
d-o-2021 <deepanshu@whozzat.com>
2024-12-02 19:22:18 +0530

    plus button modal section align to center

--- src/navigation/components/AddButton.tsx
+++ src/navigation/components/AddButton.tsx
@@ -131,7 +131,7 @@ class AddButtonOptions extends React.PureComponent<Props> {
                     withHandle={false}
                     modalStyle={styles.modalStyle}
                 >
-                    <View style={{flex:1,padding:12}}>
+                    <View style={styles.sectionContainer}>
                         <Text style={styles.listTitle}>Inventory</Text>
                         {rows.map((rowItems, rowIndex) => (
                             <View style={styles.buttonContainer} key={rowIndex}>
@@ -235,5 +235,10 @@ const styles = StyleSheet.create({
         fontSize: GD_FONT_SIZE.medium, 
         marginTop: 5, 
         color:DefaultTheme.colors.secondary
+    },
+    sectionContainer : {
+        flex:1,
+        padding:12, 
+        alignSelf:'center'
     }
 })
